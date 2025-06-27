ca18de571a4b87c774cd6fa7e38485f3b4e907f6
Gautam Singhal <singhal.gautam.gs@gmail.com>
2025-06-27 17:11:26 +0530

    corrected the name in adminHeader.jsx

--- src/Pages/HeaderFooters/Admin/AdminHeader.jsx
+++ src/Pages/HeaderFooters/Admin/AdminHeader.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { FaSignOutAlt, FaUser } from "react-icons/fa";
 import BrandLogo from "../../../Assets/GiftersLogoW.png";
-import "./adminHeader.css";
+import "./AdminHeader.css";
 import { useNavigate } from "react-router-dom";
 import { usePopup } from "../../GlobalFunctions/GlobalPopup/GlobalPopupContext";
 
